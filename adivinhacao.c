15da90b8725b98cd93337902a7c8e25e1f0817d6
edersonlrf <edersonluis.rf@gmail.com>
2016-09-24 19:07:12 -0300

    A2 condicionais e loops com ifs, fors e whiles

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 
+// O "#..." significa diretivas.
+#define NUMERO_DE_TENTATIVAS 3;
+
 int main() {
     printf("************************************\n");
     printf("* Bem vindo ao Jogo de Adivinhação *\n");
     printf("************************************\n");
-	
-	int chute;
 
-	printf("Qual é o seu chute? ");
-	scanf("%d", &chute);
-	printf("Seu chute foi %d\n", chute);
+    int chute;
+    int numerosecreto = 42;
+
+    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+        printf("Qual é o seu %do. chute? ", i);
+        scanf("%d", &chute);
+
+        if(chute < 0) {
+            printf("Você não pode chutar números negativos\n");
+            i--;
+            continue;
+        }
+
+        // A linguagem sem nao tem o tipo "boolean", então usar a convencao "int 0 == false" e "int 1 == true".
+        int acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            break;
+        } else if(maior) {
+            printf("Seu chute foi maior do que o número secreto!\n");
+        } else {
+            printf("Seu chute foi menor do que o número secreto!\n");
+        }
+    }
+
+    printf("Obrigado por jogar!\n");
 }
