51f9d23f3db6f5dde2d6433af10493e2cb5edcb8
edersonlrf <edersonluis.rf@gmail.com>
2016-09-24 20:59:12 -0300

    A3 tipos de dados e operacoes matematicas

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,4 +1,7 @@
+// Geralmente todos os programas usam esta primeira biblioteca pois ela armazena as funcoes de entrada e saida.
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // O "#..." significa diretivas. E aqui esta declarando uma CONSTANTE.
 // #define NUMERO_DE_TENTATIVAS 3;
@@ -10,26 +13,38 @@ int main() {
     printf("* Bem vindo ao Jogo de Adivinhação *\n");
     printf("************************************\n");
 
-    int numerosecreto = 42;
+    // Gera o timestamp.
+    int segundos = time(0);
+    // Coloca a semente na funcao.
+    srand(segundos);
+    // Gera um numero randomico muito grande.
+    int numerogrande = rand();
+    // Pega o resto da divisao de 1 a 99.
+    int numerosecreto = numerogrande % 100;
+    // Tudo em duas linhas:
+    // srand(time(0));
+    // int numerosecreto = rand() % 100;
 
     int chute;
+    int acertou = 0;
     int tentativas = 1;
+    double pontos = 1000;
 
-    while(1) {
+    while(!acertou) {
 
         printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
-
         scanf("%d", &chute);
-        printf("Seu chute foi %d\n", chute);
 
         if(chute < 0) {
             printf("Você não pode chutar números negativos!\n");
             continue;
         }
 
+        printf("Seu chute foi %d\n", chute);
+
         // A linguagem sem nao tem o tipo "boolean", então usar a convencao "int 0 == false" e "int 1 == true".
-        int acertou = (chute == numerosecreto);
+        acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
 
         if(acertou) {
@@ -43,8 +58,18 @@ int main() {
         }
 
         tentativas++;
+
+        // Vc precisa fazer o "casting" pq o compilador le da direita para esquerda, entao ele inseri um "int" na variavel mesmo ela sendo "double".
+        double pontosperdidos = abs(chute - numerosecreto) / (double) 2;
+        // "if" para resolver o bug do chute 30, pq o "-(-1)" dava "+" e o jogador ganhava pontos por errar. Vc tmb pode usar a funcao "abs()" que transforma tudo em positivo.
+        // if (pontosperdidos < 0) {
+        //     pontosperdidos = pontosperdidos * -1;
+        // }
+        pontos = pontos - pontosperdidos;
     }
 
-    printf("Fim de jogo!\n");
     printf("Você acertou em %d tentativas!", tentativas);
+    // O "%.2f" exibe com duas casas decimais.
+    printf("Você fez %.2f pontos!\n", pontos);
+    printf("Fim de jogo!\n");
 }
